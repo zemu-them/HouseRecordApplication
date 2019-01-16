86d6079567bc40e7f0e410493ea527c4e76d3dac
zemu-them <40880379+zemu-them@users.noreply.github.com>
2019-01-16 23:06:12 +0900

    datePickerDialog

--- HouseRecordApplication/app/src/main/java/work/zemu_them/android/houserecordapplication/MoneyRegistActivity.kt
+++ HouseRecordApplication/app/src/main/java/work/zemu_them/android/houserecordapplication/MoneyRegistActivity.kt
@@ -1,12 +1,25 @@
 package work.zemu_them.android.houserecordapplication
 
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.icu.util.Calendar
+import android.widget.DatePicker
 
 class MoneyRegistActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_money_regist)
+
+        // 日付設定ダイアログの作成
+        val datePickerDialog = DatePickerDialog(
+            this@MoneyRegistActivity
+        )
+        val datePicker = datePickerDialog.datePicker
+        // 日付設定ダイアログの表示
+        datePickerDialog.show()
+
     }
 }
